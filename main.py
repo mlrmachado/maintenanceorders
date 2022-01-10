6c455e9c6636e3e727ce75b246324ca60eec900d
Jonathan Cani Kuntz <jckzti@gmail.com>
2022-01-10 02:36:23 -0300

    Teste upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+df = pd.read_csv('GSPC.csv')
+#print(df.head())
+
+df = df.drop('Date', axis = 1) # Deleta a coluna de data "date"
+
+#print(df.head()) # Sem o campo data
+df[-2::]
+#print(df[-2::]) # retonr as últimas 2 linhas
+
+amanha = df[-1::]
+amanha
+
+base = df.drop(df[-1::].index, axis = 0)
+base.tail()
+
+base['target'] = base['Close'][1:len(base)].reset_index(drop=True) # Cria uma nova coluna, com o conteúdo da coluna CLOSE, começando do índice 1, não 0 [1:]
+base.tail()
+
+prev = base[-1::].drop('target', axis = 1)
+
+treino = base.drop(base[-1::].index, axis=0) # exclui última linha(-1)
+treino.tail()
+
+#treino.loc[treino['target'] > treino['Close'], 'target'] = 1 # se target > close na coluna, então a coluna target vira 1
+#treino.tail()
+
+#treino.loc[treino['target'] != 1, 'target'] = 0 # se o valor for diferente de 1 então vira 0
+#treino.tail()
+
+print(treino)
+
+y = treino['target']
+x = treino.drop('target', axis=1)
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.2, random_state=0)
+
+lr = LinearRegression()
+
+lr.fit(x_treino,y_treino)
+
+#from sklearn.ensemble import ExtraTreesClassifier
+#modelo = ExtraTreesClassifier()
+#modelo.fit(x_treino, y_treino)
+r_sq = lr.score(x, y)
+resultado = r_sq
+
+#resultado = modelo.score(x_teste, y_teste)
+print("Acurácia:", resultado)
+
+prev['target'] = lr.predict(prev)
+print(prev)
+
+#print(modelo.predict(prev))
